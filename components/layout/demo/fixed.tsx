1de9539cab819f6174359508a037a9896d936805
Bodhesh kumar meher <bodhesh.kumar@capsitech.com>
2025-07-07 13:29:19 +0530

    Update fixed.tsx
    Signed-off-by: Bodhesh kumar meher <bodhesh.kumar@capsitech.com>

--- components/layout/demo/fixed.tsx
+++ components/layout/demo/fixed.tsx
@@ -35,10 +35,7 @@ const App: React.FC = () => {
         />
       </Header>
       <Content style={{ padding: '0 48px' }}>
-        <Breadcrumb
-          style={{ margin: '16px 0' }}
-          items={[{ title: 'Home' }, { title: 'List' }, { title: 'App' }]}
-        />
+        
         <div
           style={{
             padding: 24,
